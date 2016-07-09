a67da72a74d43cbe03411e59cc523b6e217fc4eb
Phelipe Alves <sougyn@gmail.com>
2016-07-09 11:09:48 -0300

    Organizando projeto e add gtest

---
+++ dijkstra-heap/MinHeap.h
@@ -0,0 +1,49 @@
+#ifndef DIJKSTRA_HEAP_HEAP_H
+#define DIJKSTRA_HEAP_HEAP_H
+
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+// Prototype of a utility function to swap two integers
+void swap(int *x, int *y);
+
+// A class for Min Heap
+class MinHeap
+{
+    int *harr; // pointer to array of elements in heap
+    int capacity; // maximum possible size of min heap
+    int heap_size; // Current number of elements in min heap
+public:
+    // Constructor
+    MinHeap(int capacity);
+
+    // to heapify a subtree with root at given index
+    void MinHeapify(int );
+
+    int parent(int i) { return (i-1)/2; }
+
+    // to get index of left child of node at index i
+    int left(int i) { return (2*i + 1); }
+
+    // to get index of right child of node at index i
+    int right(int i) { return (2*i + 2); }
+
+    // to extract the root which is the minimum element
+    int extractMin();
+
+    // Decreases key value of key at index i to new_val
+    void decreaseKey(int i, int new_val);
+
+    // Returns the minimum key (key at root) from min heap
+    int getMin() { return harr[0]; }
+
+    // Deletes a key stored at index i
+    void deleteKey(int i);
+
+    // Inserts a new key 'k'
+    void insertKey(int k);
+};
+
+#endif //DIJKSTRA_HEAP_HEAP_H
--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include "HeapNode.h"
-#include "CompreMinHeap.h"
+#include <MinHeap.h>
+//#include "MinHeap.h"
 
 using namespace std;
 
 
 int main() {
-    priority_queue<HeapNode, std::vector<HeapNode>, CompareMinHeap> minHeap;
-    minHeap.push(HeapNode(1, 3));
-    minHeap.push(HeapNode(1, 4));
+//    priority_queue<HeapNode, std::vector<HeapNode>, CompareMinHeap> minHeap;
+//    minHeap.push(HeapNode(1, 3));
+//    minHeap.push(HeapNode(1, 4));
+//
+//    while (!minHeap.empty()) {
+//        cout << minHeap.top().dist << " ";
+//        minHeap.pop();
+//    }
 
-    while (!minHeap.empty()) {
-        cout << minHeap.top().dist << " ";
-        minHeap.pop();
-    }
+    MinHeap h(11);
+    h.insertKey(3);
+    h.insertKey(2);
+    h.deleteKey(1);
+    h.insertKey(15);
+    h.insertKey(5);
+    h.insertKey(4);
+    h.insertKey(45);
+    cout << h.extractMin() << " ";
+    cout << h.getMin() << " ";
+    h.decreaseKey(2, 1);
+    cout << h.getMin();
 
     return 0;
 }
