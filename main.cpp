7fca10cdf88124f44c93c70baae7b63885cb196b
Bruno Soares da Silva <brunosoares.inf@gmail.com>
2016-07-14 19:07:43 -0300

    - Aplicacao instrumentada para coletar os tempos de execucao.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,29 @@
 #include <dijkstra/Dijkstra.h>
 #include <graph/builder/DimacsGraphBuilder.h>
 
+#define DIMACS_GRAPH_FILE_PATH  "/home/bruno/Downloads/USA-road-d.NY.gr"
+#define DARYHEAP_D  4
+#define DIJKSTRA_START_NODE 1
+
 using namespace std;
 
 int main()
 {
-    Graph *graph = DimacsGraphBuilder::build("/home/bruno/Downloads/USA-road-d.NY.gr");
-    Dijkstra *dijkstra = new Dijkstra(graph, new DAryHeap(3), 1);
-    dijkstra->run();
+    clock_t start_time = clock();
+    Graph *graph = DimacsGraphBuilder::build(DIMACS_GRAPH_FILE_PATH);
+    clock_t graph_load_time = clock();
 
-    dijkstra->print();
+    Dijkstra *dijkstra = new Dijkstra(graph, new DAryHeap(DARYHEAP_D), DIJKSTRA_START_NODE);
+    clock_t dijkstra_start_time = clock();
+    dijkstra->run();
+    clock_t dijkstra_end_time = clock();
+
+    cout << "Start time: " << start_time << endl <<
+            "Graph load time: " << graph_load_time - start_time << endl <<
+            "Dijkstra run time: " << dijkstra_end_time - dijkstra_start_time << endl <<
+            "Finish time: " << dijkstra_end_time << endl;
+
+    //dijkstra->print();
     cout << "FINISHED" << endl;
     cout.flush();
 
