610feca0171fc43c0c500b30326307d9b868d7e1
Phelipe Alves de Souza <sougyn@gmail.com>
2016-07-26 23:12:09 -0300

    Update test

--- dijkstra-heap_test/heap_test/basic.cpp
+++ dijkstra-heap_test/heap_test/basic.cpp
@@ -1,10 +1,16 @@
+#include <HeapNode.h>
 #include "gtest/gtest.h"
 
 TEST(basic_check, test_eq) {
-    EXPECT_EQ(1, 0);
+    EXPECT_EQ(1, 1);
 }
 
 TEST(basic_check, test_neq) {
     EXPECT_NE(1, 0);
 }
 
+TEST(HeapNodeTest, HeapNodeEQVertexAndDistance_Test) {
+    HeapNode foo(1,4);
+    EXPECT_EQ(1, foo.getVertex());
+    EXPECT_EQ(4, foo.getDistance());
+}
